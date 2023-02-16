003774b4d29b7e0824a953b9ef8c46f8c138b56a
MD. SAYEED HASAN OVI <1805065@ugrad.cse.buet.ac.bd>
2023-02-17 01:49:29 +0600

    Delete sample.py

--- sample.py
+++
@@ -1,8 +0,0 @@
-from pdf2image import convert_from_path
-
-pages = convert_from_path('sample.pdf', 500)
-
-for i in range (len(pages)):
-    pages[i].save('page'+str(i)+'.jpg', 'JPEG')
-print("Successful!")
-
