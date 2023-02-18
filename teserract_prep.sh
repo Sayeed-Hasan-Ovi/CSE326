cf88d7b72f751e82a8b4445ab0b06fff3b4398ec
Akib <cadetakib1689@gmail.com>
2023-02-18 18:39:34 +0600

    added field upto date of birth

--- teserract_prep.sh
+++ teserract_prep.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 # File paths
-<<<<<<< HEAD
-pdf_file="input/req.pdf"
-coordinates_file="input/coordinates.txt"
-=======
-pdf_file="input/test1.pdf"
-coordinates_file="input/coordinateS.txt"
->>>>>>> f2242c9154fc3c2f10250b8dbe522d5c90c31570
+pdf_file="input/test2.pdf"
+coordinates_file="input/coordinates.csv"
 output="intermediate/"
 
 # Read the coordinates from the text file
 while IFS= read -r line; do
-    echo "$line"
     # Split the line into coordinates
     IFS=',' read -ra coord <<< "$line"
+    # if the line does not have 7 elements, skip it
+    if [ ${#coord[@]} -ne 7 ]; then
+        echo "Skipping line: $line"
+        continue
+    fi
+    # echo "Processing line: $line"
     x1=${coord[0]}
     y1=${coord[1]}
     x2=${coord[2]}
     y2=${coord[3]}
     page_num=${coord[4]}
     field_name=${coord[5]}
+    field_type=${coord[6]}
+    echo "Processing field: $field_name"
     
-    echo "output: $output"
+    # echo "output: $output"
 
     # Call the Python script to crop the image
     python3 pdf_to_img.py "$pdf_file" "$output" "$x1" "$y1" "$x2" "$y2" "$page_num" "$field_name"
