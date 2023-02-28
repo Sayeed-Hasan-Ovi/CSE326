e793be535432fb2925a05a86bb8c9f0956e455f3
Akib <cadetakib1689@gmail.com>
2023-02-28 19:43:22 +0600

    changed the sh file for ease of testing

--- teserract_prep.sh
+++ teserract_prep.sh
@@ -14,7 +14,7 @@ output="intermediate/"
 if [ ! -d "$output" ]; then
     mkdir -p "$output"
 fi
-
+flag=0
 # Read the coordinates from the text file
 while IFS= read -r line; do
     # Split the line into coordinates
@@ -22,20 +22,22 @@ while IFS= read -r line; do
     # if the line does not have 7 elements, skip it
     if [ ${#coord[@]} -ne 7 ]; then
         echo "Skipping line: $line"
+        flag=1
         continue
     fi
-    # echo "Processing line: $line"
-    x1=${coord[0]}
-    y1=${coord[1]}
-    x2=${coord[2]}
-    y2=${coord[3]}
-    page_num=${coord[4]}
-    field_name=${coord[5]}
-    field_type=${coord[6]}
-    echo "Processing field: $field_name"
-    
-    # echo "output: $output"
-
-    # Call the Python script to crop the image
-    python3 pdf_to_img.py "$pdf_file" "$output" "$x1" "$y1" "$x2" "$y2" "$page_num" "$field_name"
+    # if the flag is set then do processing
+    if [ $flag -eq 1 ]; then
+        # echo "Processing line: $line"
+        x1=${coord[0]}
+        y1=${coord[1]}
+        x2=${coord[2]}
+        y2=${coord[3]}
+        page_num=${coord[4]}
+        field_name=${coord[5]}
+        field_type=${coord[6]}
+        echo "Processing field: $field_name"
+        
+        # Call the Python script to crop the image
+        python3 pdf_to_img.py "$pdf_file" "$output" "$x1" "$y1" "$x2" "$y2" "$page_num" "$field_name"
+    fi
 done < "$coordinates_file"
