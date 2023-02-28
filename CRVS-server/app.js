0737654bb9a8596ee04d6377a9bfe3246dc1606e
RazinReaz <1805074@ugrad.cse.buet.ac.bd>
2023-02-28 23:02:50 +0600

    file upload done. renaming can be fixed. for now it renames the file with the time. also install nodemon

--- CRVS-server/app.js
+++ CRVS-server/app.js
@@ -22,13 +22,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(publicPath));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
--- CRVS-server/routes/index.js
+++ CRVS-server/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({mergeParams: true});
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+
+var pdfUploadRouter = require('./pdf/upload');
+// set up sub routers
+router.use('/pdf/upload', pdfUploadRouter);
+
+
+router.get('*', (req, res) => {
+  const data = {
+    pageTitle: '404',
+    message: 'Page not found'
+  }
+  res.status(400).render('error', data)
+})
 
 module.exports = router;
