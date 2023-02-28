71ed1c600a57b7fa018e3696e8c35bee59783ce8
Akib <cadetakib1689@gmail.com>
2023-02-28 23:49:28 +0600

    put the process of integrating the script with main codebase

--- CRVS-server/routes/index.js
+++ CRVS-server/routes/index.js
@@ -9,10 +9,17 @@ router.get('/', function(req, res, next) {
   // build the shell equivalent command for the script
   var command = './teserract_prep.sh';  
   // call the runScript function for testing
-  
-  runScript(command, 'test2.pdf')
-    .then(() => console.log('finished running script'))
-    .catch((err) => console.error(err));
+  // if the script runs successfully, 
+  // print 'finished running script' to the console
+  // redirect to final page
+  runScript(command, 'test2.pdf').then((result) => {
+    console.log('finished running script');
+    // todo : try to figure out a process of redirecting to final page
+    // res.redirect('/final');
+  }).catch((err) => {
+    console.log('error running script');
+    console.log(err);
+  });
   res.render('index', { title: 'CRVS form Uploader!' });
 });
 
